5ded148c9553327a8de6f55ae0399ded884b2b2a
Emma Yang <emmayang78@gmail.com>
2023-02-28 14:34:10 -0500

    Initial commit: : benchmarking and sbatch submission scripts with all_reduce support

---
+++ benchmark.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import time
+import torch
+import argparse
+import torch.distributed as dist
+from pathlib import Path
+from typing import Any
+from enum import Enum, auto
+from torch.cuda import Event
+
+class Collective(Enum):
+    all_reduce = 'all_reduce'
+    broadcast = 'broadcast'
+    reduce = 'reduce'
+    all_gather = 'all_gather'
+    gather = 'gather'
+    scatter = 'scatter'
+    reduce_scatter = 'reduce_scatter'
+    all_to_all = 'all_to_all'
+
+    def __str__(self) -> str:
+        return self.value
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--collective', default=Collective.all_reduce, type=Collective, choices=list(Collective),
+                        help="collective function to benchmark [all_reduce, broadcast, reduce, scatter, gather, all_gather, all_to_all, reduce_scatter]")
+    parser.add_argument('--tensor_size', default=10, type=int,
+                        help="tensor size to operate on")
+    parser.add_argument('--world_size', default=-1, type=int, 
+                        help='number of nodes for distributed training')
+    parser.add_argument('--global_rank', default=-1, type=int, 
+                        help='global node rank for distributed training')
+    parser.add_argument('--dist_url', default='env://', type=str, 
+                        help='url used to set up distributed training')
+    parser.add_argument('--dist_backend', default='nccl', type=str, 
+                        help='distributed backend')
+    parser.add_argument('--local_rank', default=-1, type=int, 
+                        help='local rank for distributed training')
+    parser.add_argument('--job_dir', default="/n/home02/emyang/collective_benchmark", type=str,
+                        help="job directory")
+    parser.add_argument('--out_dir', default="/n/home02/emyang/collective_benchmark/benchmark_results", type=str,
+                        help="output directory for benchmarking results")
+    args = parser.parse_args()
+
+    return args
+
+def print_env():
+    print("World Size: ", os.environ["WORLD_SIZE"])
+    print("Master Addr: ", os.environ["MASTER_ADDR"])
+    print("Master Port:" , os.environ["MASTER_PORT"])
+    print("Slurm Procid: ", os.environ["SLURM_PROCID"])
+
+class Task:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, args: Any) -> Any:
+
+        print_env()
+
+        if "WORLD_SIZE" in os.environ:
+            args.world_size = int(os.environ["WORLD_SIZE"])
+
+        args.distributed = args.world_size > 1
+        ngpus_per_node = torch.cuda.device_count()
+
+        if 'SLURM_PROCID' in os.environ:
+            args.global_rank = int(os.environ["SLURM_PROCID"])
+            args.local_rank = args.global_rank % ngpus_per_node
+
+            dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size, rank=args.global_rank)
+
+            torch.cuda.set_device(args.local_rank)
+            self.experiment(args)
+        return "Success"
+
+    def experiment(self, args):
+        tensor = torch.ones(args.tensor_size, device='cuda') * args.local_rank
+        if args.collective == Collective.all_reduce:
+            self.experiment_allreduce()
+        print(tensor)
+
+    def teardown(self):
+        dist.destroy_process_group()
+
+    def experiment_allreduce(self):
+        niters = 10
+
+        ######################################
+        # 1. warming up CUDACachingAllocator #
+        ######################################
+        for _ in range(10):
+            data_tensor = torch.randn(5*(2**18), dtype=torch.float32, device=args.local_rank)
+            dist.all_reduce(data_tensor)
+            data_tensor = None
+
+        # wait for all pending CUDA ops to finish
+        torch.cuda.synchronize(device=args.local_rank)
+        current_size = 0
+        size = 5*(2**18)
+        num_tasks = os.environ["WORLD_SIZE"]
+        name = f"all_red_{num_tasks}_{args.local_rank}"
+        delay_dir = f"{args.out_dir}/all_reduce"
+        Path(delay_dir).mkdir(parents=True, exist_ok=True)
+        fout = open(f"{delay_dir}/{name}.data", "w")
+        for i in range(45):
+            if(i == 20):
+                size = 20 * (2**18)
+            elif(i==30):
+                size = 50 * (2**18)
+            current_size += size
+            size_in_mb = (current_size * 4)// 2**20     
+
+            ##################################################################
+            # 2. measure raw delays and memory to rule out profiler overhead #
+            ##################################################################
+            events_pre_all_reduce = [Event(enable_timing=True) for _ in range(niters)]
+            events_post_all_reduce = [Event(enable_timing=True) for _ in range(niters)]
+            for i in range(niters):
+                data_tensor = torch.randn(current_size, dtype=torch.float32, device=args.local_rank)
+                events_pre_all_reduce[i].record()
+                dist.all_reduce(data_tensor)
+                events_post_all_reduce[i].record()
+                data_tensor = None
+
+
+            # wait for all pending CUDA ops to finish
+            torch.cuda.synchronize(device=args.local_rank)
+
+            delays_all_reduce = [pre.elapsed_time(post) for pre, post in zip(events_pre_all_reduce, events_post_all_reduce)]
+
+            # write results
+            for delay in delays_all_reduce:
+                print("writing results")
+                fout.write(
+                    f"{size_in_mb}, {delay:.4f}\n"
+                )
+           
+            # wait for all peers to finish
+            dist.barrier(device_ids=[args.local_rank])
+
+        fout.close()
+        self.teardown()
+        return {
+            "data_size" : size_in_mb,
+        }
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    task = Task()
+    task(args)
---
+++ exp.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+### This script submits a SLURM job for benchmark.py
+
+#SBATCH --job-name=nccl-benchmarking
+#SBATCH --partition=gpu_test
+#SBATCH --time=1:00:00
+
+### e.g. request 4 nodes with 1 gpu each, totally 4 gpus (WORLD_SIZE==4)
+### Note: --gres=gpu:x should equal to ntasks-per-node
+
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64gb
+### chdir specifies the path of the main file that you want to run using srun i.e. the path of 'benchmark.py' in our case
+
+#SBATCH --chdir=/n/home02/emyang/collective_benchmark
+
+### create a folder 'logs' in the scratch space of our lab: /n/holyscratch01/idreos_lab/Users/<your-username>/logs
+### %x - specifies job-name, %j - specifies job-number, %t - specifies task number
+
+#SBATCH --output=/n/holyscratch01/idreos_lab/Users/%u/job_logs/%x-%j.out
+#SBATCH --error=/n/holyscratch01/idreos_lab/Users/%u/job_logs/%x-%j.err
+#SBATCH --open-mode=append
+
+# create directory for logs for this SLURM job
+mkdir /n/holyscratch01/idreos_lab/Users/emyang/job_logs/${SLURM_JOB_ID}
+
+### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
+### change WORLD_SIZE as gpus/node * num_nodes
+export MASTER_PORT=12340
+export WORLD_SIZE=8
+
+echo "JOB ID="${SLURM_JOB_ID}
+
+### get the first node name as master address - customized for vgg slurm
+### e.g. master(gnodee[2-5],gnoded1) == gnodee2
+echo "NODELIST="${SLURM_JOB_NODELIST}
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+echo "Username="$USER
+
+#nccl variables
+export NCCL_DEBUG='INFO'
+export NCCL_DEBUG_SUBSYS='INIT,ENV,NET'
+export NCCL_IB_CUDA_SUPPORT=1
+
+### init virtual environment if needed
+# source /n/home02/emyang/.bashrc
+# source /n/idreos_lab/users/emyang/develop/initenv.sh
+
+### the command to run
+srun --output /n/holyscratch01/idreos_lab/Users/%u/job_logs/%j/%j_%t.out --error /n/holyscratch01/idreos_lab/Users/%u/job_logs/%j/%j_%t.err python3 benchmark.py --collective all_reduce --tensor_size 5
