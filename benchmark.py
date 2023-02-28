280d0acb982b8a022a8d7d95a8b1573934ce10fa
Emma Yang <emmayang78@gmail.com>
2023-02-28 16:53:45 -0500

    Added support for benchmarking reduce_scatter

--- benchmark.py
+++ benchmark.py
@@ -29,8 +29,6 @@ def parse_args():
 
     parser.add_argument('--collective', default=Collective.all_reduce, type=Collective, choices=list(Collective),
                         help="collective function to benchmark [all_reduce, broadcast, reduce, scatter, gather, all_gather, all_to_all, reduce_scatter]")
-    parser.add_argument('--tensor_size', default=10, type=int,
-                        help="tensor size to operate on")
     parser.add_argument('--world_size', default=-1, type=int, 
                         help='number of nodes for distributed training')
     parser.add_argument('--global_rank', default=-1, type=int, 
@@ -83,10 +81,10 @@ class Task:
         return "Success"
 
     def experiment(self, args):
-        tensor = torch.ones(args.tensor_size, device='cuda') * args.local_rank
         if args.collective == Collective.all_reduce:
             self.experiment_allreduce()
-        print(tensor)
+        elif args.collective == Collective.reduce_scatter:
+            self.experiment_reduce_scatter()
 
     def teardown(self):
         dist.destroy_process_group()
@@ -154,6 +152,85 @@ class Task:
 
             # write results
             for delay in delays_all_reduce:
+                print("writing results")
+                fout.write(
+                    f"{size_in_mb}, {delay:.4f}\n"
+                )
+           
+            # wait for all peers to finish
+            dist.barrier(device_ids=[args.local_rank])
+
+        fout.close()
+        self.teardown()
+        return {
+            "data_size" : size_in_mb,
+        }
+
+    def experiment_reduce_scatter(self):
+        niters = 10
+
+        ######################################
+        # 1. warming up CUDACachingAllocator #
+        ######################################
+        for _ in range(10):
+            tensor_out = torch.zeros(5*(2**18), dtype=torch.float32, device=args.local_rank)
+            tensor_in = torch.arange(5*(2**18) * args.world_size, dtype=torch.float32, device=args.local_rank)
+            dist.reduce_scatter_tensor(tensor_out, tensor_in)
+            data_tensor = None
+
+        # wait for all pending CUDA ops to finish
+        torch.cuda.synchronize(device=args.local_rank)
+        current_size = 0
+        size = 5*(2**18)
+        num_tasks = os.environ["WORLD_SIZE"]
+        name = f"red_scat_{num_tasks}_{args.local_rank}"
+        delay_dir = f"{args.out_dir}/reduce_scatter"
+        Path(delay_dir).mkdir(parents=True, exist_ok=True)
+        fout = open(f"{delay_dir}/{name}.data", "w")
+        for i in range(45):
+            if(i == 20):
+                size = 20 * (2**18)
+            elif(i==30):
+                size = 50 * (2**18)
+            current_size += size
+            size_in_mb = (current_size * 4)// 2**20     
+
+            if args.profile:
+                profile_fout = open(f"{delay_dir}/{name}.profiler.data", "w")
+
+            ##################################################################
+            # 2. measure raw delays and memory to rule out profiler overhead #
+            ##################################################################
+            events_pre_reduce_scatter = [Event(enable_timing=True) for _ in range(niters)]
+            events_post_reduce_scatter = [Event(enable_timing=True) for _ in range(niters)]
+            for i in range(niters):
+                tensor_out = torch.zeros(current_size, dtype=torch.float32, device=args.local_rank)
+                tensor_in = torch.arange(current_size * args.world_size, dtype=torch.float32, device=args.local_rank)
+                events_pre_reduce_scatter[i].record()
+
+                if args.profile:
+                    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True, 
+                    profile_memory=True, use_cuda=True) as prof:
+                        dist.reduce_scatter_tensor(tensor_out, tensor_in)
+                else:
+                    dist.reduce_scatter_tensor(tensor_out, tensor_in)
+
+                events_post_reduce_scatter[i].record()
+                torch.cuda.synchronize(device=args.local_rank)
+                data_tensor = None
+
+                if args.profile and args.local_rank == 0:
+                    profile_fout.write(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+                    # Trace for viewing in Chrome profiling tool
+                    # prof.export_chrome_trace(f"{delay_dir}/{name}.chrome_trace.sync")
+
+            # wait for all pending CUDA ops to finish
+            torch.cuda.synchronize(device=args.local_rank)
+
+            delays_reduce_scatter = [pre.elapsed_time(post) for pre, post in zip(events_pre_reduce_scatter, events_post_reduce_scatter)]
+
+            # write results
+            for delay in delays_reduce_scatter:
                 print("writing results")
                 fout.write(
                     f"{size_in_mb}, {delay:.4f}\n"
